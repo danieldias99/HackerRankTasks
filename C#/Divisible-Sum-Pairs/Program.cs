0510efaeee2c0eff68b051188a9cbd83ee1091b2
danieldias99 <56049583+danieldias99@users.noreply.github.com>
2020-11-02 16:10:36 +0000

    Divisible sum pairs in C#

---
+++ C#/Divisible-Sum-Pairs/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+
+//Divisible Sum Pairs - Hacker Rank
+
+namespace Run
+{
+    class Program
+    {
+
+        // Complete the divisibleSumPairs function below.
+        static int divisibleSumPairs(int n, int k, int[] ar)
+        {
+
+            int count_pairs = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    var sum = ar[i] + ar[j];
+                    if (sum % k == 0) //checks if (the sum) is divisible by k
+                    {
+                        count_pairs++;
+                    }
+                }
+            }
+            return count_pairs;
+        }
+
+        static void Main(string[] args)
+        {
+
+            int n = 2;
+            int k = 2;
+
+            int[] ar = new int[] {8,10};
+            int result = divisibleSumPairs(n, k, ar);
+
+            Console.Write(result);
+            /*textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();*/
+        }
+    }
+}

543d4ec55a7da487adcec895377a2ee01682ffa3
danieldias99 <56049583+danieldias99@users.noreply.github.com>
2020-11-13 00:51:21 +0000

    Bubble Sort in C#

---
+++ Sorting/Bubble-Sort/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the countSwaps function below.
+    static void countSwaps(int[] a) {
+
+        int count_swaps = 0;
+        int temp = 0;
+        int n = a.Length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - 1; j++) {
+                // Swap adjacent elements if they are in decreasing order
+                if (a[j] > a[j + 1]) {
+                    count_swaps++;
+                    temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+            }
+        }
+        Console.WriteLine("Array is sorted in " + count_swaps + " swaps.");
+        Console.WriteLine("First Element: " + a[0]);
+        Console.WriteLine("Last Element: " + a[n - 1]);
+    }
+
+    static void Main(string[] args) {
+        //int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] a = new int[]{3, 2, 1};
+
+        countSwaps(a);
+    }
+}
